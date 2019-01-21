fac9cc44609d76a0ae0bd828ebda2c4338ad6ea9
Ruby <ngoc@tinypulse.com>
2019-01-21 22:39:28 +0700

    Clean image and process extracting text

--- extracting-text-fom-images-using-ruby/read_image.rb
+++ extracting-text-fom-images-using-ruby/read_image.rb
@@ -6,6 +6,7 @@ require 'open3'
 gemfile do
   source 'https://rubygems.org'
   gem 'mini_magick', '~> 4.5', '>= 4.5.1'
+  gem 'byebug', '~> 9.0', '>= 9.0.5'
 end
 
 SRC_DIR = 'extracting-text-fom-images-using-ruby/source_images'.freeze
@@ -14,10 +15,35 @@ TMP_DIR = 'extracting-text-fom-images-using-ruby/tmp'.freeze
 Dir.mkdir TMP_DIR unless File.exists?(TMP_DIR)
 
 Pathname.new(SRC_DIR).children.each do |f|
+  # convert image to grayscale
   src_path = f.realpath
   tmp_path = "#{TMP_DIR}/#{f.basename}"
 
   img = MiniMagick::Image.open(src_path)
   img.colorspace('Gray')
   img.write(tmp_path)
+
+  # cleaning the image
+  # MiniMagick::Tool::Magick.new do |magick|
+  #   magick << tmp_path
+  #   magick.negate
+  #   magick.threshold("007%")
+  #   magick.negate
+  #   magick << tmp_path
+  # end
+  # Still dont know why MiniMagick::Tool::Magick does not work on ImageMagick 7 
+  
+  MiniMagick::Tool::Convert.new do |convert|
+    convert << tmp_path
+    convert.negate
+    convert.threshold("012%")
+    convert.negate
+    convert << tmp_path
+    convert.call
+  end
+
+  # Regconizing the text
+  puts "********* #{f.basename}  ******************"
+  text, _, _ = Open3.capture3("tesseract #{tmp_path} stdout")
+  puts text.strip
 end
---
+++ extracting-text-fom-images-using-ruby/readme.md
@@ -0,0 +1,33 @@
+## Extracting Text From Images Using Ruby
+
+1. Read all the images in source folder and convert it to grayscale image
+
+2. Cleaning the image: Negates the image to black and white to remove all the noise. ( This improve the ability of Tessertact to read the text in the image )
+
+3. Regconizing the text
+
+## Requisite
+
+brew install tesseract
+
+brew install tesseract --all-languages
+
+## What to know
+
+Tesseract is an open source text recognizer (OCR) Engine
+
+It can be used directly, or (for programmers) using an API to extract printed text from images. It supports a wide variety of language
+
+https://github.com/tesseract-ocr/tesseract/blob/master/doc/tesseract.1.asc
+
+-----
+Open3.capture3 captures the standard output and the standard error of a command.
+https://www.rubydoc.info/stdlib/open3/Open3.capture3
+
+## Ref: 
+
+https://aonemd.github.io/blog/extracting-text-from-images-using-ruby
+
+https://github.com/dannnylo/rtesseract
+
+https://www.rubydoc.info/stdlib/open3/Open3.capture3
